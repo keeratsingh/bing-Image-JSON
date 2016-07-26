58f76b3ac8135979b6962d8267fa356d14f709f2
Keerat Singh <keeratsingh@outlook.com>
2016-07-25 20:54:47 -0700

    Added method parseJSONString to extract the URL from the JSON String

--- bingImage/MainPage.xaml.cs
+++ bingImage/MainPage.xaml.cs
@@ -16,9 +16,14 @@ using Windows.UI.Xaml.Navigation;
 
 // Http Client
 using Windows.Web.Http;
-
 // Aync tasks
 using System.Threading.Tasks;
+// Parse JSON Bing String
+// using Newtonsoft.Json; // JsonConvert
+using Windows.Data.Json;
+
+// Displaying Images
+using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -37,31 +42,55 @@ namespace bingImage
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
-            string test = await getJSONString();
+            // Variables
+            int numOfImages = 3;
+            string strRegion = "en-US";
+
+            string strRawJSONString = await getJSONString(numOfImages, strRegion);
+            List<string> lstBingImageURLs = parseJSONString(numOfImages, strRawJSONString);
         }
 
-        // We need to use either void or Task if we use await in the method body.
-        public async Task<string> getJSONString()
+
+        /* 
+         * This Method fetches the JSON string from the BingAPI endpoint and then returns the Raw String to the caller
+         * We need to use either void or Task if we use await in the method body.
+        */
+        public async Task<string> getJSONString(int _numOfImages, string _strRegion)
         {
-            // Variables
-            string noOfImages = "1";
-            string region = "en-US";
+           
             // We can specify the region and number of images we want for the Bing Image of the Day.
-            string bingImageURL = string.Format("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n={0}&mkt={1}", noOfImages, region);
-            string jsonString = "";
+            string strBingImageURL = string.Format("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n={0}&mkt={1}", _numOfImages, _strRegion);
+            string strJSONString = "";
 
             // Use Windows.Web.Http Namespace as System.Net.Http will be deprecated in future versions.
             HttpClient client = new HttpClient();
 
+            // Using an Async call makes sure the app is responsive during the time the response is fetched.
             // GetAsync sends a Async GET request to the Specified URI
-            HttpResponseMessage response = await client.GetAsync(new Uri(bingImageURL));
+            HttpResponseMessage response = await client.GetAsync(new Uri(strBingImageURL));
 
             // Content property get or sets the content of a HTTP response message
             // ReadAsStringAsync is a method of the HttpContent which asynchronously reads the content of the HTTP Response and returns as a string
-            jsonString = await response.Content.ReadAsStringAsync();
+            strJSONString = await response.Content.ReadAsStringAsync();
 
-            return jsonString;
+            return strJSONString;
+        }
 
+        /*
+         * This Method parses the fetched JSON string and retrieves the Image URLs using Microsoft Windows.Data.Json class
+         * Each Url is stored as a separate List item and the list of URLs is returned to the caller  
+         */
+        public List<string> parseJSONString(int _numOfImages, string _strRawJSONString)
+        {
+            List<string> _lstBingImageURLs = new List<string>(_numOfImages);
+            JsonObject jsonObject = JsonObject.Parse(_strRawJSONString);
+
+            for(int i=0; i < _numOfImages; i++)
+            {
+                _lstBingImageURLs.Add( jsonObject["images"].GetArray()[i].GetObject()["url"].GetString() );
+            }
+
+            return _lstBingImageURLs;
         }
     }
 }

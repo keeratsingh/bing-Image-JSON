32ee7c75f008df8e8fac20d9547999a0e7efe6e5
Keerat Singh <keeratsingh@outlook.com>
2016-07-27 23:03:21 -0700

    Added Comments

--- bingImage/MainPage.xaml.cs
+++ bingImage/MainPage.xaml.cs
@@ -36,7 +36,7 @@ namespace bingImage
         }
 
         /* 
-         * This Method fetches the JSON string from the BingAPI endpoint and then returns the Raw String to the caller
+         * This Method fetches the JSON string from the BingAPI endpoint and then returns the Raw String to the caller.
          * We need to use either void or Task if we use await in the method body.
         */
         public async Task<string> getJSONString(int _numOfImages)
@@ -51,11 +51,11 @@ namespace bingImage
             HttpClient client = new HttpClient();
 
             // Using an Async call makes sure the app is responsive during the time the response is fetched.
-            // GetAsync sends a Async GET request to the Specified URI
+            // GetAsync sends a Async GET request to the Specified URI.
             HttpResponseMessage response = await client.GetAsync(new Uri(strBingImageURL));
 
-            // Content property get or sets the content of a HTTP response message
-            // ReadAsStringAsync is a method of the HttpContent which asynchronously reads the content of the HTTP Response and returns as a string
+            // Content property get or sets the content of a HTTP response message.
+            // ReadAsStringAsync is a method of the HttpContent which asynchronously reads the content of the HTTP Response and returns as a string.
             strJSONString = await response.Content.ReadAsStringAsync();
 
             return strJSONString;
@@ -63,25 +63,30 @@ namespace bingImage
 
         /*
          * This Method parses the fetched JSON string and retrieves the Image URLs using Microsoft Windows.Data.Json class
-         * Each Url is stored as a separate List item and the list of URLs is returned to the caller .
+         * Each Url is stored as a separate List item and the list of URLs is returned to the caller.
         */
         public List<string> parseJSONString(int _numOfImages, string _strRawJSONString)
         {
             List<string> _lstBingImageURLs = new List<string>(_numOfImages);
-            // JsonObject class implements the IMap interface, which helps in manipulating the name/value pairs like a dictionary
+            // JsonObject class implements the IMap interface, which helps in manipulating the name/value pairs like a dictionary.
             JsonObject jsonObject;
             
-            // JsonObject.TryParse parses the JSON string into a JSON value
-            // TryParse returns a boolean value, indicating success or failure
+            // JsonObject.TryParse parses the JSON string into a JSON value, which returns a boolean value, indicating success or failure.
+            // TryParse is an added safe measure to avoid an execption while parsing.
             bool boolParsed = JsonObject.TryParse(_strRawJSONString, out jsonObject);
             if (boolParsed)
             {
                 for (int i = 0; i < _numOfImages; i++)
                 {
+                    // The retrieval structure depends upon JSON string, the base key in our case is "images".
+                    // If you retrieve more than one image, "images" key will have more than one array values.
+                    // Each Array value has a key/value pair "url", which we retrieve and conver it to a string.
                     _lstBingImageURLs.Add(jsonObject["images"].GetArray()[i].GetObject()["url"].GetString());
                 }
             }
 
+            // Just reverse the order of the retrieved Images, just to have a better visibility when the number of images change.
+            _lstBingImageURLs.Reverse();
             return _lstBingImageURLs;
         }
 
@@ -91,33 +96,46 @@ namespace bingImage
         */
         public void displayImages(List<string> _lstBingImageURLs)
         {
-            // Clear the Stack Panel and start afresh
+            // Clear the Stack Panel in order to avoid duplication and start afresh.
             if (spImages != null)
                 spImages.Children.Clear();
 
             foreach (string url in _lstBingImageURLs)
             {
+                // We use the Image control to display an image on the UI.
                 Image imgbingImage = new Image();
                 var bingURL = "https://www.bing.com" + url;
-                ImageSource imgbingImageSource = new BitmapImage(new Uri(bingURL));
-                imgbingImage.Source = imgbingImageSource;
-                spImages.Children.Add(imgbingImage);
 
+                // BitmapImage class inherits from the BitmapSource class and is specialized BitmapSource that is
+                // optimized for loading images using XAML.
+                // WPF essentially runs a background thread that downloads and save the image in memory before it is displayed.
+                BitmapSource imgbingImageSource = new BitmapImage(new Uri(bingURL));
+                imgbingImage.Source = imgbingImageSource;
+                // Add the Image control to the Stack Panel
+                spImages.Children.Add(imgbingImage);
             }
         }
 
 
        /*
-        * This Method updates the UI based on the value of the slider
+        * This Method updates the UI based on the value of the slider.
        */
         private async void sldNoOfImages_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
-           
-            int _numOfImages = Convert.ToInt32( e.NewValue );
-            string strRawJSONString = await getJSONString(_numOfImages);
-            List<string> lstBingImageURLs = parseJSONString(_numOfImages, strRawJSONString);
-            displayImages(lstBingImageURLs);
-
+            try
+            {
+                int _numOfImages = Convert.ToInt32(e.NewValue);
+                string strRawJSONString = await getJSONString(_numOfImages);
+                List<string> lstBingImageURLs = parseJSONString(_numOfImages, strRawJSONString);
+                displayImages(lstBingImageURLs);
+                txtStatus.Text = "";
+            }
+            
+            // Display an exception message
+            catch(Exception ex)
+            {
+                txtStatus.Text = ex.Message;
+            }
         }
     }
 }
